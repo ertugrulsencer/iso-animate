963fbfdab65508c743dcd09bd667fc91413e91fa
ertugrulencer <teknoertugrul@gmail.com>
2021-09-25 01:38:18 +0300

    Finish 1.0.0

---
+++ js/iso.animate.js
@@ -0,0 +1,21 @@
+const select = (el, cb) => {
+  document.querySelectorAll(el).forEach(cb);
+};
+const observer = new IntersectionObserver(function (entries) {
+  entries.forEach((entrie) => {
+    if (entrie.isIntersecting) {
+      entrie.target.classList.add("iso-animate-active");
+    }
+  });
+});
+select(".iso-animate", (el) => {
+  if (!el.dataset.isoAnimate) el.dataset.isoAnimate = "fade";
+  if (!el.dataset.isoAnimateOrigin) el.dataset.isoAnimateOrigin = "left";
+  if (!el.dataset.isoAnimateDuration) el.dataset.isoAnimateDuration = 350;
+  let { isoAnimate, isoAnimateOrigin, isoAnimateDuration } = el.dataset;
+  isoAnimateDuration = Number(isoAnimateDuration);
+  el.style.setProperty("--duration", `${isoAnimateDuration}ms`);
+  el.parentElement.style.overflow = "hidden";
+  observer.observe(el);
+  console.log(isoAnimate, isoAnimateOrigin);
+});
