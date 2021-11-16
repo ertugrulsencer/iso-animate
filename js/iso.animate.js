a347a4743d09c98197e5e69b27452c6f578f09f6
ertugrulencer <teknoertugrul@gmail.com>
2021-11-16 14:29:53 +0300

    Add transition delay

--- js/iso.animate.js
+++ js/iso.animate.js
@@ -12,9 +12,13 @@ select(".iso-animate", (el) => {
   if (!el.dataset.isoAnimate) el.dataset.isoAnimate = "fade";
   if (!el.dataset.isoAnimateOrigin) el.dataset.isoAnimateOrigin = "left";
   if (!el.dataset.isoAnimateDuration) el.dataset.isoAnimateDuration = 350;
-  let { isoAnimate, isoAnimateOrigin, isoAnimateDuration } = el.dataset;
+  if (!el.dataset.isoAnimateDelay) el.dataset.isoAnimateDelay = 0;
+  let { isoAnimate, isoAnimateOrigin, isoAnimateDuration, isoAnimateDelay } =
+    el.dataset;
   isoAnimateDuration = Number(isoAnimateDuration);
+  isoAnimateDelay = Number(isoAnimateDelay);
   el.style.setProperty("--duration", `${isoAnimateDuration}ms`);
+  el.style.setProperty("--delay", `${isoAnimateDelay}ms`);
   el.parentElement.style.overflow = "hidden";
   observer.observe(el);
   console.log(isoAnimate, isoAnimateOrigin);
